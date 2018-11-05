7feff42d9a7fadadf80182e229f72d366d354254
IAN <IAN@192.168.18.17>
2018-11-05 23:53:51 +0800

    exam practice

---
+++ myproject/src/com/Ian/Midtermexamreview.java
@@ -0,0 +1,26 @@
+package com.Ian;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Midtermexamreview {
+
+	public static void main(String[] args) {
+		/*
+		 * System.out.println("I hate,Midtermexams!");
+		 * System.out.println("I\nhate\n\"Final exams\",\ntoo!");
+		 * System.out.println("Actually,\tas long as there are \"exams\"\nI HATE IT! ");
+		 * int a=5; int b=a; a=3; System.out.println(a==b); String line="I am a boy";
+		 * System.out.println(line);
+		 */
+		double r=123456788;
+		if(r%2==0) {
+			System.out.printf("r is even");
+			
+		}else {
+			System.out.printf("r is odd");
+		}
+
+	}
+
+}
--- myproject/src/com/Ian/猜數字.java
+++ myproject/src/com/Ian/猜數字.java
@@ -5,24 +5,41 @@ import java.util.Scanner;
 public class ²q¼Æ¦r {
 
 	public static void main(String[] args) {
-		
+
 		Scanner s = new Scanner(System.in);
 		System.out.println("Please guess a number bewteen 1 and 100 :");
 		int n = s.nextInt();
-		if (n >= 25) {
-			System.out.println("Please guess a number bewteen 25 and 50:");
-			int number2 = s.nextInt();
-			if (n >=40) {
-				System.out.println("Please guess a number bewteen 40 and 60 :");
-				int number3 = s.nextInt();
-				if (n >= 55) {
-					System.out.println("Please guess a number bewteen 55 and 60 :");
-					int number4 = s.nextInt();
-					if (n == 59) {
-						System.out.println("BINGO!!!:");
-					} else {
-						System.out.println("Please try again");
+		if (n < 25) {
+			System.out.println("Please try again");
+		} else {
 
+			if (n >= 25) {
+				System.out.println("Please guess a number bewteen 25 and 50:");
+				int n2 = s.nextInt();
+				if (n <40) {
+					System.out.println("Please try again");
+				} else {
+					if (n >= 40) {
+						System.out.println("Please guess a number bewteen 40 and 60 :");
+						int n3 = s.nextInt();
+						if (n <55) {
+							System.out.println("Please try again");
+						} else {
+							if (n >= 55) {
+								System.out.println("Please guess a number bewteen 55 and 60 :");
+								int n4 = s.nextInt();
+								if (n < 58) {
+									System.out.println("Please try again");
+								} else {
+									if (n == 59) {
+										int n5 = s.nextInt();
+										System.out.println("BINGO!!!:");
+									} else {
+										System.out.println("Please try again");
+									}
+								}
+							}
+						}
 					}
 				}
 			}
